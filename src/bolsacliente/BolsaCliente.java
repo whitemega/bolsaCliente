a3f498f78c6cb328ac0f60a138c142157ab1874b
Sabrina <sabrina@Melissa.(none)>
2011-10-23 12:19:52 -0430

    Para todo lo demas existe el terminal

---
+++ src/bolsacliente/BolsaCliente.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bolsacliente;
+
+/**
+ *
+ * @author sabrina
+ */
+public class BolsaCliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
